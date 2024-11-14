15f67714d9d7f6b07e78643a4dca6dc21345c963
Chris Shewfelt <chris@apromoplace.com>
2024-11-14 01:50:51 +0000

    Project Complete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //assign variables
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".io", ".org", ".net", ".us"];
+  let domainArray = [];
+  // create loops to generate strings
+  for (const wordProNoun of pronoun) {
+    for (const wordAdj of adj) {
+      for (const wordNoun of noun) {
+        for (const wordExt of ext) {
+          let completeString = `${wordProNoun}${wordAdj}${wordNoun}${wordExt}`;
+          domainArray.push(completeString);
+          console.log(completeString);
+        }
+      }
+    }
+  }
+  //document.querySelector("#domainName").innerHTML = domainArray;
+  const map1 = domainArray.map(x => `<li class="list-group-item">${x}</li>`);
+  document.querySelector("#domainList").innerHTML = map1.join("");
 };
